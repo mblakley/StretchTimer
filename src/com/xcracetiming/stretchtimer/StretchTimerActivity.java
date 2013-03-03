9c286ea39380feec8c06ff65774c1227b363f953
mblakley <mark.blakley@gmail.com>
2013-03-02 22:20:34 -0500

    First crack at some Dependency Injection using RoboGuice as the IoC container

---
+++ src/com/xcracetiming/stretchtimer/StretchTimerActivity.java
@@ -0,0 +1,10 @@
+package com.xcracetiming.stretchtimer;
+
+import com.example.stretchtimer.R;
+
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+
+@ContentView(R.layout.activity_stretch_timer)
+public class StretchTimerActivity extends RoboActivity {
+}
